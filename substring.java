ee07d643195a4f630e29ce13054778ee83a31ef0
karan-patidar8839 <144881344+karan-patidar8839@users.noreply.github.com>
2023-09-15 08:56:33 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,10 @@
+class substring{
+    public static void main(String[]args){
+    String first = "java";
+    System.out.println("first String :" + first);
+    String second= "programming";
+    System.out.println("second String :" +second);
+    first.substring(0,4);
+    System.out.println(first.substring(0,4));
+}
+}
