4fa732315563b944947636e3a56630b27eea5955
karan-patidar8839 <144881344+karan-patidar8839@users.noreply.github.com>
2023-09-15 08:57:16 +0530

    Add files via upload

---
+++ string in java.java.java
@@ -0,0 +1,12 @@
+// to create string in java
+public class Main
+{
+	public static void main(String[] args) {
+	    String first= "java";
+	    String second = "python";
+	    String third = " javascript";
+		System.out.println("first");
+		System.out.println("second");
+		System.out.println("third");
+	}
+}
