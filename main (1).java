95093754bf254b60ed0465f49299ab9aed6e723b
karan-patidar8839 <144881344+karan-patidar8839@users.noreply.github.com>
2023-09-15 08:57:33 +0530

    Add files via upload

---
+++ main (1).java
@@ -0,0 +1,10 @@
+class main{
+    public static void main(String[]args){
+    String first = "java";
+    System.out.println("first String"+first);
+    String second= "programming";
+    System.out.println("second String"+first);
+    String joinedString = first.concat(second);
+    System.out.println("joined String"+joinedString);
+}
+}
